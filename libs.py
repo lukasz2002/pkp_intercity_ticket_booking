03574f1950463a180fb7b82ba78a15915ea23fb1
Łukasz Kleina <lukasz.kleina02@gmail.com>
2024-02-14 20:10:37 +0100

    added getting date provided by user into dict and validating that date

--- libs.py
+++ libs.py
@@ -8,15 +8,17 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from datetime import date
+import calendar
 
 
 class Web:
     def __init__(self):
         # self.login = input("Pass email: ")
         # self.password = input("Pass password: ")
-        self.start_station = input("Pass start station: ")
-        self.destination_station = input("Pass destination station: ")
-        # self.date = input("Pass date in format DD-MM-YY: ")
+        # self.start_station = input("Pass start station: ")
+        # self.destination_station = input("Pass destination station: ")
+        self.date = input("Pass date in format DD-MM-YYYY: ")
         # self.time = input("Pass when train should start from start station (format: HH): ")
         # self.preferred_ticket_class = input("What is your preferred class? First -> 1, Second -> 2: ")
         # self.reduced_tariff = input("Any tariff? ")
@@ -31,7 +33,7 @@ class Web:
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
 
-    def pass_station_and_date(self):
+    def pass_station(self):
         inputs = {
             'start_station': '//*[@id="stname-0"]',
             'final_station': '//*[@id="stname-1"]',
@@ -57,3 +59,60 @@ class Web:
             .key_down(Keys.ARROW_DOWN) \
             .key_down(Keys.ENTER) \
             .perform()
+
+    def get_user_date_into_dict(self):
+        date_as_list = self.date.split("-")
+        if len(date_as_list) != 3:
+            # TODO: raise error here
+            print("Wrong date format")
+            exit()
+
+        for piece_of_date in date_as_list:
+            if not piece_of_date.isdigit():
+                # TODO: raise error here
+                print("Letters were used!")
+                exit()
+
+        date_dict = {
+            "day": int(date_as_list[0]),
+            "month": int(date_as_list[1]),
+            "year": int(date_as_list[2]),
+        }
+
+        lower_year_limit = date.today().year
+        upper_year_limit = date.today().year + 1
+        self.check_lower_date_limit(date_dict["year"], lower_year_limit, "year")
+        self.check_upper_date_limit(date_dict["year"], upper_year_limit, "year")
+
+        is_current_year = date_dict["year"] == lower_year_limit
+
+        if is_current_year:
+            lower_month_limit = date.today().month
+            upper_month_limit = 12
+            self.check_lower_date_limit(date_dict["month"], lower_month_limit, "month")
+            self.check_upper_date_limit(date_dict["month"], upper_month_limit, "month")
+            lower_day_limit = date.today().day
+            upper_day_limit = calendar.monthrange(date_dict["year"], date_dict["month"])[1]
+            self.check_lower_date_limit(date_dict["day"], lower_day_limit, "day")
+            self.check_upper_date_limit(date_dict["day"], upper_day_limit, "day")
+        else:
+            upper_month_limit = date.today().month
+            self.check_upper_date_limit(date_dict["month"], upper_month_limit, "month")
+            upper_day_limit = date.today().day
+            self.check_upper_date_limit(date_dict["day"], upper_day_limit, "day")
+
+        return date_dict
+
+    @staticmethod
+    def check_upper_date_limit(user_date, limit, msg):
+        if user_date > limit:
+            # TODO: raise error here
+            print(f"Provided {msg} is too large!")
+            exit()
+
+    @staticmethod
+    def check_lower_date_limit(user_date, limit, msg):
+        if user_date < limit:
+            # TODO: raise error here
+            print(f"Provided {msg} is too small!")
+            exit()
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from libs import Web
 
 def run():
     web = Web()
-    web.init_web()
-    web.pass_station_and_date()
+    # web.init_web()
+    # web.pass_station()
+    # web.pass_date()
+    web.get_user_date_into_dict()
 
 
 if __name__ == "__main__":
